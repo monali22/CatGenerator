c98686ce4b7624d7cc1dbd9a775a92a15cbbc5fe
Monali Chandurkar <chandurkar.monali123@gmail.com>
2020-03-20 01:06:44 -0700

    done cat generator

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function generate () {
+    var image = document.createElement("img");
+    var div = document.getElementById("flex-box-gen");
+    image.setAttribute("src","./assets/images/cat1.gif");
+    div.appendChild(image);
+}
